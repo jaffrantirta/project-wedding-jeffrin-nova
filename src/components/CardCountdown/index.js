18d402174b9796279d95fe4aadb81d669a46d748
jaffrantirta <franartika@gmail.com>
2023-02-22 17:52:58 +0800

    update cardcountdown

--- src/components/CardCountdown/index.js
+++ src/components/CardCountdown/index.js
@@ -1,8 +1,31 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 export default function CardCountdown({ text, targetDate, value }) {
     const sectionRef = useRef(null)
     const [isVisible, setIsVisible] = useState(false)
+    const calculateTimeLeft = useCallback(() => {
+        const difference = +new Date(targetDate) - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60)
+            };
+        } else {
+            timeLeft = {
+                days: '00',
+                hours: '00',
+                minutes: '00',
+                seconds: '00'
+            };
+        }
+
+        return timeLeft;
+    }, [targetDate])
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
     useEffect(() => {
         let node = sectionRef.current;
         const observer = new IntersectionObserver(
@@ -31,31 +54,7 @@ export default function CardCountdown({ text, targetDate, value }) {
             }
             clearInterval(intervalId)
         };
-    }, [sectionRef, targetDate])
-
-    const calculateTimeLeft = () => {
-        const difference = +new Date(targetDate) - +new Date();
-        let timeLeft = {};
-
-        if (difference > 0) {
-            timeLeft = {
-                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                minutes: Math.floor((difference / 1000 / 60) % 60),
-                seconds: Math.floor((difference / 1000) % 60)
-            };
-        } else {
-            timeLeft = {
-                days: '00',
-                hours: '00',
-                minutes: '00',
-                seconds: '00'
-            };
-        }
-
-        return timeLeft;
-    }
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+    }, [sectionRef, targetDate, calculateTimeLeft])
 
     const addLeadingZeros = value => {
         value = String(value);
--- src/components/EventDetails/index.js
+++ src/components/EventDetails/index.js
@@ -31,29 +31,29 @@ export default function EventDetails() {
     return (
         <section ref={sectionRef} className={`h-fit font-primary p-2 md:p-10 origin-center flex flex-col gap-3 justify-center items-center text-primary`}>
             <div className={`text-sm md:text-xl border-2 border-primary p-2 md:p-10 rounded-3xl text-center w-5/6 shadow-2xl`}>
-                <p className={`mb-7`}>Merupakan suatu kehormatan dan kebahagiaan kami apabila Bapak/Ibu/Saudara/i berkenan hadir memberikan doa restu.</p>
-                <p className={`text-2xl md:text-6xl font-third mb-7`}>Resepsi Pawiwahan</p>
-                <p className={`text-sm md:text-xl`}>Minggu, 16 April 2023</p>
-                <p className={`text-sm md:text-xl`}>Pukul 13.00 WITA - Selesai</p>
-                <p className={`text-sm md:text-xl mb-7`}>Br. Apuan, Desa Apuan, Kecamatan Baturiti, Tabanan</p>
-                <button className={`px-6 py-3 rounded-full shadow-xl bg-primary text-white`}>Map Lokasi Acara</button>
+                <p className={`mb-7 transition-all duration-1000 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Merupakan suatu kehormatan dan kebahagiaan kami apabila Bapak/Ibu/Saudara/i berkenan hadir memberikan doa restu.</p>
+                <p className={`text-2xl md:text-6xl font-third mb-7 transition-all duration-700 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Resepsi Pawiwahan</p>
+                <p className={`text-sm md:text-xl transition-all duration-500 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Minggu, 16 April 2023</p>
+                <p className={`text-sm md:text-xl transition-all duration-300 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Pukul 13.00 WITA - Selesai</p>
+                <p className={`text-sm md:text-xl transition-all duration-200 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'} mb-7`}>Br. Apuan, Desa Apuan, Kecamatan Baturiti, Tabanan</p>
+                <button className={`px-6 py-3 rounded-full shadow-xl bg-primary text-white transition-all duration-150 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Map Lokasi Acara</button>
             </div>
             <div className={`text-sm md:text-xl p-2 md:p-10 rounded-3xl text-center w-5/6`}>
                 <div className={`mb-5`}>
-                    <p className={`font-third text-2xl md:text-5xl`}>Protokol Kesehatan</p>
-                    <p>Tanpa mengurangi rasa hormat, dikarenakan situasi yang sedang terjadi ditengah pandemi covid-19 ini kami memohon untuk wajib mematuhi seluruh protokol kesehatan yang ada di lokasi acara.</p>
+                    <p className={`font-third text-2xl md:text-5xl transition-all duration-1000 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Protokol Kesehatan</p>
+                    <p className={`transition-all duration-1000 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>Tanpa mengurangi rasa hormat, dikarenakan situasi yang sedang terjadi ditengah pandemi covid-19 ini kami memohon untuk wajib mematuhi seluruh protokol kesehatan yang ada di lokasi acara.</p>
                 </div>
                 <div className={`grid grid-cols-3 gap-5`}>
-                    <div className={`grid justify-center`}>
-                        <img src={ImgCuciTangan} className={`w-auto h-auto`} />
+                    <div className={`grid justify-center transition-all duration-700 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>
+                        <img alt='cuci-tangan' src={ImgCuciTangan} className={`w-auto h-auto`} />
                         <p>Cuci Tangan</p>
                     </div>
-                    <div className={`grid justify-center`}>
-                        <img src={ImgJagaJarak} className={`w-auto h-auto`} />
+                    <div className={`grid justify-center transition-all duration-500 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>
+                        <img alt='jaga-jarak' src={ImgJagaJarak} className={`w-auto h-auto`} />
                         <p>Jaga Jarak</p>
                     </div>
-                    <div className={`grid justify-center`}>
-                        <img src={ImgPakaiMasker} className={`w-auto h-auto`} />
+                    <div className={`grid justify-center transition-all duration-300 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'}`}>
+                        <img alt='pakai-masker' src={ImgPakaiMasker} className={`w-auto h-auto`} />
                         <p>Pakai Masker</p>
                     </div>
                 </div>

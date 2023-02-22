8839bddfb8ff8fb3b3325116f07b1c92ffe52461
jaffrantirta <franartika@gmail.com>
2023-02-23 01:39:23 +0800

    update wish

--- src/components/WishingAndGreeting/index.js
+++ src/components/WishingAndGreeting/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { API } from 'aws-amplify'
 
 export default function WishingAndGreeting() {
+    const addComment = async () => {
+        try {
+            const response = await API.post('api', '/comments', { body: 'hai' });
+            console.log('Comment created:', response);
+        } catch (error) {
+            console.error('Error creating comment:', error);
+        }
+    }
     return (
         <section>
-
+            <button onClick={() => addComment()}>add</button>
         </section>
     )
 }
